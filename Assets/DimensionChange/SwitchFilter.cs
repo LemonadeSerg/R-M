4038b45571adf0fe2ae2915f9a4e8552d97355f3
Chris <chris.jon.sharp@gmail.com>
2020-02-15 07:09:37 +0000

    Git INIT
    Some basic multidimensional puzzles
    visual aspect of portals working

---
+++ Assets/DimensionChange/SwitchFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchFilter : MonoBehaviour
+{
+
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ToggleCull("BLUE-World");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ToggleCull("RED-World");
+        }
+    }
+
+
+    private void ToggleCull(string layer)
+    {
+        camera.cullingMask ^= 1 << LayerMask.NameToLayer(layer);
+    }
+     
+
+}
---
+++ Assets/Portal/Portal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public MeshRenderer side1, side2;
+
+    public GameObject otherPortal;
+    public GameObject player;
+    RenderTexture renderTexture;
+    public Camera cam;
+    public Transform playerCamera;
+    public GameObject pTracker;
+    void Start()
+    {
+        renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cam.targetTexture = renderTexture;
+        side1.material.mainTexture = renderTexture;
+        side2.material.mainTexture = renderTexture;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        pTracker.transform.position = playerCamera.transform.position;
+        pTracker.transform.rotation = playerCamera.transform.rotation;
+        cam.transform.localPosition = pTracker.transform.localPosition;
+        cam.transform.localRotation = pTracker.transform.localRotation;
+    }
+}
